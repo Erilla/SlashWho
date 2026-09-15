9cb67c6d99a064fe2e7c407e3459ea887ff899f7
Ryan Wong <me@ryanwong.uk>
2026-09-15 09:06:23 +0100

    Compact manual connected-character controls

--- apps/web/src/components/dossier-raid-list.tsx
+++ apps/web/src/components/dossier-raid-list.tsx
@@ -350,6 +350,7 @@ function KillEvidence({ boss }: { boss: KillBoss }) {
                         <DossierParseList
                           label={`${isChronologicalFirst ? "First kill" : "Kill"} parses`}
                           parses={evidence.parses}
+                          showCharacterName={false}
                         />
                       </dd>
                     </div>
